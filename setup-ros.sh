9f2da1c647feef4336427d988750c4bf2afca7ff
DCZhiRong <104623231+DCZhiRong@users.noreply.github.com>
2026-02-12 10:56:25 +0800

    Update setup-ros.sh

--- setup-ros.sh
+++ setup-ros.sh
@@ -2,7 +2,5 @@
 ROS_DISTRO=$(ls /opt/ros)
 
 sudo apt update
-sudo apt install ros-${ROS_DISTRO}-leo -y
-sudo apt install ros-${ROS_DISTRO}-realsense2-description -y
 sudo apt install ros-${ROS_DISTRO}-rqt-image-view -y
 sudo apt install ros-${ROS_DISTRO}-compressed-image-transport -y
